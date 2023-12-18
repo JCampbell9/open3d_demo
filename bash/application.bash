1d39847a065a60c70689283b9834f73bff2e1600
jmcampbell <joshua.campbell@swri.org>
2023-12-18 21:11:18 +0000

    updates needed

--- bash/application.bash
+++ bash/application.bash
@@ -3,6 +3,6 @@
 export ROS_IP=172.31.1.150
 export ROS_MASTER_URI=http://$ROS_IP:11311
 source /opt/ros/noetic/setup.bash
-source /home/open3d_demo/.bashrc
-source /home/open3d_demo/open3d_demo_ws/devel/setup.bash
+source ~/.bashrc
+source ~/tpr/tpr_ws/devel/setup.bash
 roslaunch open3d_demo demo.launch sim_robot:=false
