eaed5eaf883c83b79c5392ddc0c0e837387763a1
jmcampbell <joshua.campbell@swri.org>
2023-12-20 18:24:19 -0600

    updated paths for the scannig

--- bash/start_capture.bash
+++ bash/start_capture.bash
@@ -7,8 +7,8 @@ translation_distance: 0.0
 rotational_distance: 0.0
 live: true
 tsdf_params:
-  voxel_length: 0.002
+  voxel_length: 0.006
   sdf_trunc: 0.007
   min_box_values: {x: 0.0, y: 0.0, z: 0.0}
   max_box_values: {x: 0.0, y: 0.0, z: 0.0}
-rgbd_params: {depth_scale: 1000.0, depth_trunc: 0.5, convert_rgb_to_intensity: false}"
+rgbd_params: {depth_scale: 1000.0, depth_trunc: 0.75, convert_rgb_to_intensity: false}"
