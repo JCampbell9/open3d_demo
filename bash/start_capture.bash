10e107098671c13d4c1d1d1723c33d2c1e04de62
jmcampbell <joshua.campbell@swri.org>
2023-12-21 15:41:00 -0600

    updates to get working scans

--- bash/start_capture.bash
+++ bash/start_capture.bash
@@ -8,7 +8,7 @@ rotational_distance: 0.0
 live: true
 tsdf_params:
   voxel_length: 0.006
-  sdf_trunc: 0.007
+  sdf_trunc: 0.01
   min_box_values: {x: 0.0, y: 0.0, z: 0.0}
   max_box_values: {x: 0.0, y: 0.0, z: 0.0}
 rgbd_params: {depth_scale: 1000.0, depth_trunc: 0.75, convert_rgb_to_intensity: false}"
